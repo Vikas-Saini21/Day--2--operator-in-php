c32fa1cc7301216746dc3c0fd359b1e660a7ce09
Vikas Saini <154893113+Vikas-Saini21@users.noreply.github.com>
2024-09-18 19:31:54 +0530

    Add files via upload

---
+++ Day4--operator.php
@@ -0,0 +1,32 @@
+<!-- Airthmetic/Mathematical Operator -->
+<?php
+$x=1000;
+$y=50;
+// Additon
+echo $x+$y . "<br>";
+// Subtraction
+echo $x-$y . "<br>";
+// Multiplication
+echo $x*$y . "<br>";
+// Division
+echo $x/$y . "<br>";
+// Modulus
+echo $x%$y . "<br>";
+?>
+
+
+<!-- comparsion operator -->
+<?php
+$x=1000;
+$y=50;
+//greater than
+var_dump ($x>$y);
+//less than
+var_dump ($x<$y);
+//equal to
+var_dump ($x==$y);
+//greater than equal
+var_dump ($x>=$y);
+//less than equal
+var_dump ($x<=$y);
+?>
