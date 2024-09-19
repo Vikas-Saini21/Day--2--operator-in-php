c96fc04688dbd84d3a9d4ab0d0dd2eddae497460
Vikas Saini <154893113+Vikas-Saini21@users.noreply.github.com>
2024-09-19 13:05:37 +0530

    Operator.php

--- Day4--operator.php
+++ Day4--operator.php
@@ -30,3 +30,62 @@ var_dump ($x>=$y);
 //less than equal
 var_dump ($x<=$y);
 ?>
+
+<!-- biwise operator -->
+ <!-- bitwise and -->
+<?php
+$a=23;
+$b=16;
+echo $a&$b;
+?>
+
+<!-- bitwise or -->
+<?php
+$a=23;
+$b=16;
+echo $a|$b;
+?>
+
+<!-- bitwise Xor -->
+<?php
+$a=23;
+$b=16;
+echo $a^$b;
+?>
+
+<!-- bitwise compliment -->
+<?php
+$a=23;
+echo ~$b;
+?>
+
+<!-- bitwise left shift  -->
+<?php
+$a=23;
+echo $b<<1;
+?>
+
+<!-- bitwise right shift -->
+<?php
+$a=23;
+echo $b>>1;
+?>
+
+<!-- logical operator -->
+<!-- logical and  -->
+<?php
+$a=23;
+echo $a>10 && $a>5;
+?> 
+
+<!-- logical or -->
+<?php
+$a=23;
+echo $a>10 || $a<5;
+?>
+
+<!-- logical not -->
+<?php
+$a=23;
+echo !($a<10 && $a>5);
+?>  
